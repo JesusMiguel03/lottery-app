1b3d5594abb8978e13844a28b28fa382eb903cf4
JesusMiguel03 <jmdefreitas10@gmail.com>
2025-01-06 19:20:44 -0400

    feat: add db backup

--- config/filesystems.php
+++ config/filesystems.php
@@ -40,7 +40,7 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
             'throw' => false,
         ],
@@ -56,6 +56,12 @@ return [
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
             'throw' => false,
         ],
+        'backups' => [
+            'driver' => 'local',
+            'root' => storage_path('app/private/backups'),
+            'serve' => true,
+            'throw' => false,
+        ]
 
     ],
 
