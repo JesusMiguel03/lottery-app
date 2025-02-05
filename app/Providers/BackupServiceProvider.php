fd23838db85ec910656740aa009fe2b9c104c76a
JesusMiguel03 <jmdefreitas10@gmail.com>
2025-02-04 22:43:55 -0400

    feat(backup): implement backup functionality with scheduled database backups

---
+++ app/Providers/BackupServiceProvider.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Providers;
+
+use App\Models\Backup;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\ServiceProvider;
+
+class BackupServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        $today = Carbon::now()->toDateString();
+
+        $backupLog = Backup::firstOrNew([]);
+
+        if ($backupLog->executed_at !== $today) {
+            Artisan::call('backup:run --only-db');
+
+            $backupLog->executed_at = $today;
+            $backupLog->save();
+        }
+    }
+}
